e45f227de5927b3b0ae577b2beddc91780eeb0c0
nomadbitcoin <yanluizcoito@gmail.com>
2020-04-30 19:21:15 -0300

    .

--- futebol_virtual/new_get_odds.py
+++ futebol_virtual/new_get_odds.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[284]:
+# In[1]:
 
 
 import requests
@@ -20,45 +20,9 @@ pd.options.display.max_columns = 100
 mundial = 'https://www.betfair.com/sport/virtuals/football-world-cup'
 
 
-# In[3]:
-
-
-
-
-
 # ### IRA PEGAR OS DADOS DO JOGO ATUAL
 
-# In[342]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[340]:
-
-
-
-
-
-# In[324]:
-
-
-
-
-
-# In[349]:
+# In[3]:
 
 
 def saveDataToDF():
@@ -105,19 +69,19 @@ def saveDataToDF():
     return data
 
 
-# In[326]:
+# In[4]:
 
 
 # data_event = pd.DataFrame(columns=colunas, dtype=str)
 
 
-# In[ ]:
+# In[5]:
 
 
 data_event = pd.read_csv('data_event.csv')
 
 
-# In[360]:
+# In[8]:
 
 
 while True:
@@ -130,64 +94,67 @@ while True:
     hora = informacoes.text.replace('\n','')
     event_id = informacoes.get('data-eventid')
 
-    if event_id not in data['event_id'].tolist():
-        
-        tabela_dados = evento_atual.find('div',{'class':'content eventid-' + str(event_id)})
-    
-        event_name = tabela_dados.find('div',{'class':'event-name'}).text
+    if event_id not in data_event['event_id'].tolist():
+        try:
 
-        probabilidades = tabela_dados.find('div',{'class':'market type-match_odds'}).find_all('div',{'class':'bet-button'})
-        time_A = probabilidades[0].text.replace('\n','')
-        empate = probabilidades[1].text.replace('\n','')
-        time_B = probabilidades[2].text.replace('\n','')
+            tabela_dados = evento_atual.find('div',{'class':'content eventid-' + str(event_id)})
 
-        under_over = tabela_dados.find('div',{'class':'market type-over_under_25'}).find_all('div',{'class':'bet-button'})
-        under = under_over[0].text.replace('\n','')
-        over = under_over[1].text.replace('\n','')
+            event_name = tabela_dados.find('div',{'class':'event-name'}).text
 
-        resultado_correto = tabela_dados.find('div',{'class':'market market-correct-score type-correct_score'}).find_all('div',{'class':'cell'})
+            probabilidades = tabela_dados.find('div',{'class':'market type-match_odds'}).find_all('div',{'class':'bet-button'})
+            time_A = probabilidades[0].text.replace('\n','')
+            empate = probabilidades[1].text.replace('\n','')
+            time_B = probabilidades[2].text.replace('\n','')
 
-        #para os resultados do time 1
-        resultado_1_0 = resultado_correto[1].find('span',{'class':'ui-runner-price'}).text.replace('\n','')
-        resultado_2_0 = resultado_correto[2].find('span',{'class':'ui-runner-price'}).text.replace('\n','')
-        resultado_2_1 = resultado_correto[3].find('span',{'class':'ui-runner-price'}).text.replace('\n','')
+            under_over = tabela_dados.find('div',{'class':'market type-over_under_25'}).find_all('div',{'class':'bet-button'})
+            under = under_over[0].text.replace('\n','')
+            over = under_over[1].text.replace('\n','')
 
-        #para os empates
-        resultado_0_0 = resultado_correto[5].find('span',{'class':'ui-runner-price'}).text.replace('\n','')
-        resultado_1_1 = resultado_correto[6].find('span',{'class':'ui-runner-price'}).text.replace('\n','')
-        resultado_2_2 = resultado_correto[7].find('span',{'class':'ui-runner-price'}).text.replace('\n','')
+            resultado_correto = tabela_dados.find('div',{'class':'market market-correct-score type-correct_score'}).find_all('div',{'class':'cell'})
 
-        #para os resultados do time 2
-        resultado_0_0 = resultado_correto[9].find('span',{'class':'ui-runner-price'}).text.replace('\n','')
-        resultado_0_1 = resultado_correto[10].find('span',{'class':'ui-runner-price'}).text.replace('\n','')
-        resultado_1_2 = resultado_correto[11].find('span',{'class':'ui-runner-price'}).text.replace('\n','')
+            #para os resultados do time 1
+            resultado_1_0 = resultado_correto[1].find('span',{'class':'ui-runner-price'}).text.replace('\n','')
+            resultado_2_0 = resultado_correto[2].find('span',{'class':'ui-runner-price'}).text.replace('\n','')
+            resultado_2_1 = resultado_correto[3].find('span',{'class':'ui-runner-price'}).text.replace('\n','')
 
-        dupla_chance = tabela_dados.find('div',{'class':'market type-double_chance'}).find_all('div',{'class':'bet-button'})
-        visitado_empate = dupla_chance[0].text.replace('\n','')
-        visitado_visitante = dupla_chance[1].text.replace('\n','')
-        empate_visitante = dupla_chance[2].text.replace('\n','')
+            #para os empates
+            resultado_0_0 = resultado_correto[5].find('span',{'class':'ui-runner-price'}).text.replace('\n','')
+            resultado_1_1 = resultado_correto[6].find('span',{'class':'ui-runner-price'}).text.replace('\n','')
+            resultado_2_2 = resultado_correto[7].find('span',{'class':'ui-runner-price'}).text.replace('\n','')
 
-        total_gols = tabela_dados.find('div',{'class':'market type-total_goals'}).find_all('div',{'class':'bet-button'})
-        total_0 = total_gols[0].text.replace('\n','')
-        total_1 = total_gols[1].text.replace('\n','')
-        total_2 = total_gols[2].text.replace('\n','')
-        total_3 = total_gols[3].text.replace('\n','')
-        total_4 = total_gols[4].text.replace('\n','')
-        
-        
-        saveDataToDF()
-        
-        data_event = data_event.append(data)
-        data_event.to_csv('data_event.csv', index=None)
-        print('dados salvos {}'.format(datetime.now().strftime('%H:%M:%S')))
+            #para os resultados do time 2
+            resultado_0_1 = resultado_correto[9].find('span',{'class':'ui-runner-price'}).text.replace('\n','')
+            resultado_0_2 = resultado_correto[10].find('span',{'class':'ui-runner-price'}).text.replace('\n','')
+            resultado_1_2 = resultado_correto[11].find('span',{'class':'ui-runner-price'}).text.replace('\n','')
+
+            dupla_chance = tabela_dados.find('div',{'class':'market type-double_chance'}).find_all('div',{'class':'bet-button'})
+            visitado_empate = dupla_chance[0].text.replace('\n','')
+            visitado_visitante = dupla_chance[1].text.replace('\n','')
+            empate_visitante = dupla_chance[2].text.replace('\n','')
+
+            total_gols = tabela_dados.find('div',{'class':'market type-total_goals'}).find_all('div',{'class':'bet-button'})
+            total_0 = total_gols[0].text.replace('\n','')
+            total_1 = total_gols[1].text.replace('\n','')
+            total_2 = total_gols[2].text.replace('\n','')
+            total_3 = total_gols[3].text.replace('\n','')
+            total_4 = total_gols[4].text.replace('\n','')
+
+
+            saveDataToDF()
+
+            data_event = data_event.append(data)
+            data_event.to_csv('data_event.csv', index=None)
+            print('dados salvos {}'.format(datetime.now().strftime('%H:%M:%S')))
+        except AttributeError:
+            time.sleep(10)
     else:
-        time.sleep(90)
+            time.sleep(90)
 
 
 # In[ ]:
 
 
-
+data_event
 
 
 # In[ ]:
